e2d96841e772269b91f568cb87c43f9e08fed63c
Crispthofer A. Rincon M <alex877@gmail.com>
2022-05-24 02:13:02 -0500

    [Add] Data table was added with column name and user actions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap'
+
 
 function App() {
+
+  const urlDB="https://localhost:5001/api/ManagerDB"
+  const [data, SetData]= useState([]);
+
+  const fetchGet= async ()=> {
+    await axios.get(urlDB).then(res=> {
+      // Success Status
+      SetData(res.data);
+    }).catch(error=>{
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    fetchGet();
+  }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Picture</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        {data.map(manage=>(
+          <tr key={manage.id}>
+            <td>{manage.id}</td>
+            <td>{manage.name}</td>
+            <td>{manage.category}</td>
+            <td>{manage.description}</td>
+            <td>{manage.picture}</td>
+            <td>
+              <button className="btn btn-primary">Edit</button> {" "}
+              <button className="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
